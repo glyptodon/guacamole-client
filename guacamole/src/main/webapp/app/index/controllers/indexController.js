d2f85e17e3f1d0af4fed4333b8de755d057122eb
Virtually Nick <necouchman@users.noreply.github.com>
2019-09-29 20:32:50 -0400

    GUACAMOLE-817: Merge reset keyboard state on navigation and form submission.
    (cherry picked from commit bad20e1a6cd56453f2ee1c27255bb3485925b437)

--- guacamole/src/main/webapp/app/index/controllers/indexController.js
+++ guacamole/src/main/webapp/app/index/controllers/indexController.js
@@ -125,6 +125,12 @@ angular.module('index').controller('indexController', ['$scope', '$injector',
         keyboard.reset();
     };
 
+    // Release all keys upon form submission (there may not be corresponding
+    // keyup events for key presses involved in submitting a form)
+    $document.on('submit', function formSubmitted() {
+        keyboard.reset();
+    });
+
     /**
      * Checks whether the clipboard data has changed, firing a new
      * "guacClipboard" event if it has.
@@ -163,6 +169,11 @@ angular.module('index').controller('indexController', ['$scope', '$injector',
         $scope.loginHelpText = error.translatableMessage;
         $scope.acceptedCredentials = parameters;
         $scope.expectedCredentials = error.expected;
+    });
+
+    // Ensure new pages always start with clear keyboard state
+    $scope.$on('$routeChangeStart', function routeChanging() {
+        keyboard.reset();
     });
 
     // Update title and CSS class upon navigation
